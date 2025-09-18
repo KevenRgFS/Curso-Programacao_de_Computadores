4ff07863c27c398ad9e7305cfccf1f133e82145b
KevenRgFS <kevenregiofs@gmail.com>
2025-09-18 15:23:06 -0300

    Dicionário

---
+++ Dicionário/Q001.py
@@ -0,0 +1,26 @@
+"""Registro de Eventos: Crie uma lista chamada eventos_esportivos que
+contenha dicionários. Cada dicionário deve representar um evento com
+as chaves: "nome", "modalidade" e "participantes". A chave
+"participantes" deve ser uma lista de strings com os nomes das pessoas.
+Adicione pelo menos 3 eventos à lista."""
+
+eventos_esportivos = [
+
+    {
+        "nome": "Dia do Esporte",
+        "modalidade": "Fotebol",
+        "participantes": ["6° ano", "8° ano", "9° ano", "3° série-EM"]
+    },
+
+    {
+        "nome": "Campeonato InterEscola",
+        "modalidade": "Vôlei",
+        "participantes": ["CEPI José de Assis", "Colégio Estadual Salomão Elias Abdon", "Colégio Padrão"]
+    },
+
+    {
+        "nome": "Maratona de SADE",
+        "modalidade": "Maratona 25km",
+        "participantes": ["Beatriz Evangelista", "Gabriella Braga", "João Henrique", "Lucas Domingues", "Laura Mavalli", "Marcos Uchoa"]
+    }
+]
---
+++ Dicionário/Q002.py
@@ -0,0 +1,32 @@
+"""Consulta de Estoque: Você tem um dicionário chamado estoque. As
+chaves são os nomes de produtos e os valores são dicionários com as
+informações "quantidade" e "preco". Crie um programa que faça o
+seguinte:
+o Adicione um novo produto ao estoque.
+o Verifique se um produto específico (ex: "maçã") existe no estoque
+e imprima sua quantidade."""
+
+estoque = {    
+    
+    "vaso":  
+    {
+
+    "quantidade": 20,
+    "preço": "R$16.40"
+    },
+
+    "panela": {
+
+        "quantidade": 15,
+        "preço": "R$79.99"
+    },
+
+    "garrafa": {
+
+        "quantidade": 31,
+        "preço": "R$34.00"
+    }
+}
+
+
+
